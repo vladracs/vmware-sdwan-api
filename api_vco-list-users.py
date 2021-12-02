8b5bbddbb7a8edf3ac21544f32824a4a21b7d990
Vladimir franca de sousa <vfrancad@gmail.com>
2021-12-02 10:44:35 +0100

    Create api_vco-list-users.py
    For partner/operator users to get a list of all super users in the vmware sd-wan orchestrator

---
+++ api_vco-list-users.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# Author: vfrancadesou@vmware.com
+# vfrancad@gmail.com
+
+# Pre-requisites:
+# VMware SD-WAN Orchestrator Target
+#   Enterprise admin account
+#   Enterprise user and user API token
+#
+# Not to be considered as best practices in using VMware VCO API
+# Meant to be used in Lab environments - Please test it and use at your own risk
+#
+# please note that VMWare API and Support team - do not guarantee this samples
+# It is provided - AS IS - i.e. while we are glad to answer questions about API usage
+# and behavior generally speaking, VMware cannot and do not specifically support these scripts
+#
+# Compatible with api v1 of the vmware sd-wan vco api
+# using tokens to authenticate
+
+import os
+import sys
+import requests
+import json
+
+token = "Token %s" %(os.environ['VCO_TOKEN'])
+VCO_FQDN=os.environ['VCO_HOSTNAME']
+vco_url = 'https://' + VCO_FQDN + '/portal/rest/'
+headers = {"Content-Type": "application/json", "Authorization": token}
+get_enterprises = vco_url + 'network/getNetworkEnterprises'
+list_users = vco_url + 'enterprise/getEnterpriseUsers'
+
+def main():
+	enterprises = requests.post(get_enterprises, headers=headers, data='',verify=False)
+	ent_dict = enterprises.json()
+	userents = 0
+	print('There are %d enterprises to check.  This will take approximately %d seconds' %(len(ent_dict), (len(ent_dict)/3)))
+	for enterprise in ent_dict:
+		eid = enterprise['id']
+		params = {'enterpriseId': eid}
+		userlist = requests.post(list_users, headers=headers, data=json.dumps(params),verify=False)
+		result = userlist.json()
+		for user in result:
+			if user['roleName'] == "Enterprise Superuser":
+				print('User %s (email = %s) exists in enterprise "%s" with enterprise ID %d' %(user['username'], user['email'],enterprise['name'], eid))
+if __name__ == '__main__':
+    main()
