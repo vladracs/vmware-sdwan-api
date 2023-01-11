ceb08df9650c81c4c5ea884287d3d7f3e24d7ab7
Vladimir franca de sousa <vfrancad@gmail.com>
2023-01-11 20:34:16 +0100

    Update api_vco_msp_grab_ofc.py

--- api_vco_msp_grab_ofc.py
+++ api_vco_msp_grab_ofc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # Simple Python Script for MSP users that leverages vmware sd-wan orchestrator api to read OFC route table of a specfic Customer
-#
+# 
 # usage: python3 api_vco_grab_ofc_routes -e Customer1 -r 500
 #
 # Not to be considered as best practices in using VMware VCO API
@@ -30,8 +30,10 @@ headers = {"Content-Type": "application/json", "Authorization": token}
 ######## VCO API methods
 get_edge = vco_url + 'edge/getEdge'
 get_enterprise = vco_url + 'enterprise/getEnterprise'
+get_enterprise_operator = vco_url + 'network/getNetworkEnterprises'
 get_enterprises = vco_url + 'enterpriseProxy/getEnterpriseProxyEnterprises'
 get_ofclist = vco_url+'enterprise/getEnterpriseRouteTable'
+get_ent = vco_url+'enterpriseProxy/getEnterpriseProxy'
 ######## VCO FUNCTIONS
 #### RETRIEVE ENTERPRISE ID for this user
 def find_velo_enterpriseId():
@@ -46,12 +48,22 @@ def find_velo_enterpriseId():
 def find_customer_enterpriseId(customer):
 	#Fetch enterprise id convert to JSON
     eid=0
-    enterprises = requests.post(get_enterprises, headers=headers, data='',verify=False)
-    print (enterprises)
+    enterprises = requests.post(get_enterprises, headers=headers, data='')
     ent_dict = enterprises.json()
     for enterprise in ent_dict:
         print (enterprise)
         if(enterprise['name']==customer):
+            eid=enterprise['id']
+    print('Enterprise Id = %d'%(eid))
+    return eid
+#### RETRIEVE ENTERPRISE ID for a customer - as operator
+def op_find_customer_enterpriseId(customer):
+	#Fetch enterprise id convert to JSON
+    eid=0
+    enterprises = requests.post(get_enterprise_operator, headers=headers, data='')
+    ent_dict = enterprises.json()
+    for enterprise in ent_dict:
+          if(enterprise['name']==customer):
             eid=enterprise['id']
     print('Enterprise Id = %d'%(eid))
     return eid
@@ -65,10 +77,19 @@ def main():
     args = parser.parse_args()
     customer=args.enterprise
     n_of_routes=args.routes
-    eid = find_customer_enterpriseId(customer)
+    #test if token / api access is working
+    print("Testing if token / api access is working")
+    params= {'enterpriseProxyId':1}
+    if(str(requests.post(get_enterprises, headers=headers, data=json.dumps(params))).rfind("200")!=1):
+        print("Api access works!")
+        print("Looking for %s id"%customer)
+        eid = op_find_customer_enterpriseId(customer)
+    else:
+        print("Issues accessing VCO API - troubleshoot needed")
+        exit
     if(eid!=0):
         params = {'filter':{'limit':n_of_routes,'rules':[]},'enterpriseId': eid}
-        response = requests.post(get_ofclist, headers=headers, data=json.dumps(params),verify=False)
+        response = requests.post(get_ofclist, headers=headers, data=json.dumps(params))
         resp_dict=response.json()
         print(resp_dict["subnets"])
     else:
