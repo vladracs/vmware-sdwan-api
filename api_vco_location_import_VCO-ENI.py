016871bddad21e8d8111b9d087d8f4f55b574d99
Vladimir franca de sousa <vfrancad@gmail.com>
2023-03-14 08:44:16 +0100

    Update api_vco_location_import_VCO-ENI.py

--- api_vco_location_import_VCO-ENI.py
+++ api_vco_location_import_VCO-ENI.py
@@ -2,6 +2,13 @@
 # author: vfrancadesou@vmware.com
 # Simple Python Script that uses vmware sd-wan orchestrator api to create a csv file with edge list ready for ENI import
 #
+# Not to be considered as best practices in using VMware VCO API
+# Meant to be used in Lab environments - Please test it and use at your own risk
+#
+# please note that VMWare API and Support team - do not guarantee this samples
+# It is provided - AS IS - i.e. while we are glad to answer questions about API usage
+# and behavior generally speaking, VMware cannot and do not specifically support these scripts
+#
 # if running as operator use:
 # python3 api_vco-to-eni-OP.py -o -c "Enterprise-Customer1"
 #
